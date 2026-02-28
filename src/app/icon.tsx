5c7e3c33c61decc321bde8da923ad871ab232370
Pushkar <pushkardeshpande8055@gmail.com>
2026-03-01 03:52:02 +0530

    feat: Add custom graduation cap favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,35 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    fontSize: 22,
+                    background: "#09090b", // Tailwind background / zinc-950
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "white",
+                    borderRadius: "20%",
+                }}
+            >
+                🎓
+            </div>
+        ),
+        { ...size }
+    );
+}
